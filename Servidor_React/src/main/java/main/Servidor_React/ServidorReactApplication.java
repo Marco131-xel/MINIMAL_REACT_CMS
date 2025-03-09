7eb9bdbb57aaaf3af1698c46134c9cbaba339778
Marco <marcopoolchichesapon@gmail.com>
2025-03-08 18:57:55 -0600

    Iniciando proyecto

---
+++ Servidor_React/src/main/java/main/Servidor_React/ServidorReactApplication.java
@@ -0,0 +1,13 @@
+package main.Servidor_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServidorReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServidorReactApplication.class, args);
+	}
+
+}
---
+++ Servidor_React/src/test/java/main/Servidor_React/ServidorReactApplicationTests.java
@@ -0,0 +1,13 @@
+package main.Servidor_React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServidorReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
